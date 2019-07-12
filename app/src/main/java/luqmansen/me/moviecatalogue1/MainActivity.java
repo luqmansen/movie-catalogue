98db441ef0ab48558f5f2bb08358ed7e8542eef6
luqmansen <luqmansen@gmail.com>
2019-07-12 21:33:33 +0700

    added 2 bottom navigation menu

--- app/src/main/java/luqmansen/me/moviecatalogue1/MainActivity.java
+++ app/src/main/java/luqmansen/me/moviecatalogue1/MainActivity.java
@@ -1,64 +1,42 @@
 package luqmansen.me.moviecatalogue1;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.widget.ListView;
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity
-{
-    public String[] dataTitle;
-    public String[] dataRelease;
-    public String[] dataDescription;
-    public TypedArray dataBg;
-    public String[] dataMovieTrailerId;
-    private MovieAdapter adapter;
-    private ArrayList<Movie> movies;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_movie);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_tvshows);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
-
-        adapter = new MovieAdapter( this );
-        final ListView listView = findViewById( R.id.listviewID );
-        listView.setAdapter( adapter );
-
-        prepare();
-        addItem();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-
-
-    private void addItem()
-    {
-        movies = new ArrayList<>(  );
-
-        for (int i = 0; i < dataTitle.length; i++)
-        {
-            Movie movie = new Movie(  );
-            movie.setTitle( dataTitle[i] );
-            movie.setRelease( dataRelease[i] );
-            movie.setDesc( dataDescription[i] );
-            movie.setMovieBg( dataBg.getResourceId( i, -1 ) );
-            movie.setMovieTrailerId( dataMovieTrailerId[i] );
-            movies.add( movie );
-        }
-        adapter.setMovies( movies );
-    }
-
-    public void prepare()
-    {
-        dataTitle = getResources().getStringArray( R.array.movie_title );
-        dataRelease = getResources().getStringArray( R.array.movie_release );
-        dataDescription = getResources().getStringArray( R.array.movie_desc );
-        dataBg = getResources().obtainTypedArray( R.array.movie_bg );
-        dataMovieTrailerId = getResources().getStringArray(R.array.youtube_id);
-    }
 }
---
+++ app/src/main/java/luqmansen/me/moviecatalogue1/MainActivity2.java
@@ -0,0 +1,64 @@
+package luqmansen.me.moviecatalogue1;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class MainActivity2 extends AppCompatActivity
+{
+    public String[] dataTitle;
+    public String[] dataRelease;
+    public String[] dataDescription;
+    public TypedArray dataBg;
+    public String[] dataMovieTrailerId;
+    private MovieAdapter adapter;
+    private ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main2);
+
+        adapter = new MovieAdapter( this );
+        final ListView listView = findViewById( R.id.listviewID );
+        listView.setAdapter( adapter );
+
+        prepare();
+        addItem();
+    }
+
+
+
+    private void addItem()
+    {
+        movies = new ArrayList<>(  );
+
+        for (int i = 0; i < dataTitle.length; i++)
+        {
+            Movie movie = new Movie(  );
+            movie.setTitle( dataTitle[i] );
+            movie.setRelease( dataRelease[i] );
+            movie.setDesc( dataDescription[i] );
+            movie.setMovieBg( dataBg.getResourceId( i, -1 ) );
+            movie.setMovieTrailerId( dataMovieTrailerId[i] );
+            movies.add( movie );
+        }
+        adapter.setMovies( movies );
+    }
+
+    public void prepare()
+    {
+        dataTitle = getResources().getStringArray( R.array.movie_title );
+        dataRelease = getResources().getStringArray( R.array.movie_release );
+        dataDescription = getResources().getStringArray( R.array.movie_desc );
+        dataBg = getResources().obtainTypedArray( R.array.movie_bg );
+        dataMovieTrailerId = getResources().getStringArray(R.array.youtube_id);
+    }
+}
